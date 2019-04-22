0c5e0ce1a2982c2441d15d61bf5ad307ed553dbb
daiiz <da.iz.mg@gmail.com>
2019-04-22 14:49:19 +0900

    Initial Commit

---
+++ HelpfeelSkeleton2/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  HelpfeelSkeleton2
+//
+//  Created by daiki on 2019/04/22.
+//  Copyright © 2019 daiiz. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ HelpfeelSkeleton2Tests/HelpfeelSkeleton2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HelpfeelSkeleton2Tests.swift
+//  HelpfeelSkeleton2Tests
+//
+//  Created by daiki on 2019/04/22.
+//  Copyright © 2019 daiiz. All rights reserved.
+//
+
+import XCTest
+@testable import HelpfeelSkeleton2
+
+class HelpfeelSkeleton2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
